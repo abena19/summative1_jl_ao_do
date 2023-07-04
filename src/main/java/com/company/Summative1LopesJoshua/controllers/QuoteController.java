72bff4135ed79b597dc222eb9a031b2632d8f769
abena19 <ofosuabena19@gmail.com>
2023-07-04 13:49:17 -0600

    set up skeleton for Quote model and controller

--- src/main/java/com/company/Summative1LopesJoshua/controllers/QuoteController.java
+++ src/main/java/com/company/Summative1LopesJoshua/controllers/QuoteController.java
@@ -1,4 +1,22 @@
 package com.company.Summative1LopesJoshua.controllers;
 
+import com.company.Summative1LopesJoshua.models.Quote;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class QuoteController {
+
+    @RequestMapping(value = "/quote", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public Quote getRandomQuote() {
+        Quote randomQuote = new Quote();
+        return randomQuote;
+    }
+
 }
--- src/main/java/com/company/Summative1LopesJoshua/models/Quote.java
+++ src/main/java/com/company/Summative1LopesJoshua/models/Quote.java
@@ -1,4 +1,15 @@
 package com.company.Summative1LopesJoshua.models;
 
 public class Quote {
+    private int id;
+    private String author;
+    private String quote;
+
+    public Quote() {}
+
+    public Quote(String author, String quote, int id) {
+        this.author = author;
+        this.quote = quote;
+        this.id = id;
+    }
 }
